5fb71ac96d580fdcb3ed56da95d7221dfcb42da0
Darrel Herbst <dherbst@gmail.com>
2019-10-26 09:33:41 -0400

    Next command added

--- client.go
+++ client.go
@@ -1,6 +1,14 @@
 package septa
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
 
 // Client is used to make calls to the septa website.
 type Client struct {
@@ -10,4 +18,55 @@ type Client struct {
 
 	// Timeout is the number of seconds before the call times out.
 	Timeout time.Duration
+}
+
+// NewClient creates a Client with the given domain to make api calls.
+func NewClient(domain string) *Client {
+	if domain == "" {
+		domain = "www3.septa.org"
+	}
+	c := &Client{Domain: domain}
+	return c
+}
+
+// NextToArriveResult is a line in the NTA response.
+type NextToArriveResult struct {
+	Train           string `json:"orig_train"`
+	Line            string `json:"orig_line"`
+	DepartureString string `json:"orig_departure_time"`
+	ArrivalString   string `json:"orig_arrival_time"`
+	Delay           string `json:"orig_delay"`
+	IsDirect        string `json:"isdirect"`
+}
+
+// NextToArrive makes a call to the next to arrive api and returns the results
+func (c *Client) NextToArrive(from string, to string, num int) ([]NextToArriveResult, error) {
+	url := fmt.Sprintf("https://%s/hackathon/NextToArrive/%s/%s/%d",
+		c.Domain,
+		url.PathEscape(from),
+		url.PathEscape(to),
+		num)
+	log.Printf("url=%v\n", url)
+
+	var results []NextToArriveResult
+	response, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error calling NextToArrive err=%v\n", err)
+		return results, err
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error reading response.Body %v\n", err)
+		return results, err
+	}
+	err = json.Unmarshal(body, &results)
+	if err != nil {
+		log.Printf("Error unmarshalling results %v\n%v", err, string(body))
+		return results, err
+	}
+
+	return results, nil
+
 }
--- cmd/septa/next.go
+++ cmd/septa/next.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/dherbst/septa"
 )
 
 // NextToArrive takes a "from" station name and a "to" station name and returns the expected trains.
@@ -11,4 +12,16 @@ func NextToArrive(ctx context.Context) {
 	from := flag.Arg(1)
 	to := flag.Arg(2)
 	fmt.Printf("from=%v to=%v\n", from, to)
+
+	client := septa.NewClient("")
+	results, err := client.NextToArrive(from, to, 5)
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+		return
+	}
+
+	for _, r := range results {
+		fmt.Printf("%v\n", r)
+	}
+	fmt.Printf("\nDone\n\n")
 }
